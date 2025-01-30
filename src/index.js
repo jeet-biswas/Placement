e9d17fca8121303d22223f93d08b111f5d36141b
Jeet Biswas <jeetbiswas816@gmail.com>
2025-01-31 02:29:10 +0530

    Updated build for deployment

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Register the service worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(() => console.log("Service Worker Registered"))
+      .catch((error) => console.log("Service Worker Registration Failed", error));
+  });
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
